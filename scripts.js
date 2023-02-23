8118521912c515271b2b17e888c050c0d7145747
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-23 11:33:11 -0300

    Add more information

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
 'use strict'
 
 const request = new XMLHttpRequest()
-const url = 'http://ip-api.com/xml/'
+const endpoint = 'http://ip-api.com/json/'
+const lang = 'pt-BR'
+let fields = 'status,message,country,regionName,city,isp,mobile,proxy,query'
+fields = 254489
+const url = `${endpoint}?fields=${fields}&lang=${lang}`
 request.open('GET', url)
+request.send()
 
 request.addEventListener('load', function () {
-  console.log(this.responseText)
+  const data = JSON.parse(this.responseText)
+  console.log(data)
+  if (data.proxy) {
+    console.log('Você está usando vpn ou proxy.')
+  } else {
+    console.log('Você não está usando vpn ou proxy.')
+  }
+  if (data.mobile) {
+    console.log('Você acessou o site através de um smartphone.')
+  } else {
+    console.log('Você acessou o site através de um computador.')
+  }
+  const displayText = `Seu ip é ${data.query}. Você está em ${data.city}, ${data.regionName}, ${data.country}. Seu provedor de internet é ${data.isp}.`
+  console.log(displayText)
 })
-
-request.send()
