901bde2740f29e69f1b0244ff2c287343584c136
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-24 00:33:40 -0300

    Display errors, fix bugs, add search on key press

--- scripts.js
+++ scripts.js
@@ -9,26 +9,39 @@ const displayInfo = (query) => {
     const cityCard = document.querySelector('.city')
     const countryCard = document.querySelector('.country')
     const ispCard = document.querySelector('.isp')
+    const errorCard = document.querySelector('.error')
     if (error) {
-      console.log(error)
+      ipCard.textContent = ''
+      vpnCard.textContent = ''
+      mobileCard.textContent = ''
+      cityCard.textContent = ''
+      countryCard.textContent = ''
+      ispCard.textContent = ''
+      errorCard.textContent = error
     } else {
+      errorCard.textContent = ''
       ipCard.textContent = response.query
-      console.log(response)
       ipCard.textContent = `Ip: ${response.query}`
 
       if (response.proxy) {
-        vpnCard.textContent = 'Você está usando vpn ou proxy.'
+        vpnCard.textContent = 'Você está usando vpn ou proxy'
       } else {
-        vpnCard.textContent = 'Você não está usando vpn ou proxy.'
+        vpnCard.textContent = 'Você não está usando vpn ou proxy'
       }
       if (response.mobile) {
         mobileCard.textContent = 'Você acessou o site através de um smartphone'
       } else {
         mobileCard.textContent = 'Você acessou o site através de um computador'
       }
-      countryCard.textContent = `Você está em ${response.country}`
-      cityCard.textContent = `Sua cidade é ${response.city}`
-      ispCard.textContent = `Seu provedor de internet é ${response.isp}.`
+      if (query) {
+        countryCard.textContent = `O endereço procurado está no país: ${response.country}`
+        cityCard.textContent = `O endereço procurado está na cidade: ${response.city}`
+        ispCard.textContent = `O provedor de internet do endereço procurado é: ${response.isp}.`
+      } else {
+        countryCard.textContent = `Você está no país: ${response.country}`
+        cityCard.textContent = `Sua cidade é: ${response.city}`
+        ispCard.textContent = `Seu provedor de internet é: ${response.isp}`
+      }
     }
   })
 }
@@ -39,4 +52,8 @@ const queryInput = document.querySelector('#query-input')
 
 searchButton.addEventListener('click', () => {
   displayInfo(queryInput.value)
+})
+
+queryInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') displayInfo(queryInput.value)
 })
